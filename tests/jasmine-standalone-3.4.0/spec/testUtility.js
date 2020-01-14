a65e6f9087aa870234747e970c0306eff2e33a41
ofietze <oliver.fietze@akamu.de>
2020-01-14 16:36:19 +0100

    Add tests for utility function

--- tests/jasmine-standalone-3.4.0/spec/testUtility.js
+++ tests/jasmine-standalone-3.4.0/spec/testUtility.js
@@ -1,15 +1,36 @@
 describe("utility", function(){
-  it("white should have a utility of -8", function() {
-    var chess = new Chess("8/8/8/8/8/3q4/3P4/8");
+  it("base board has utility of 0", function() {
+    var chess = new Chess();
     chess.turn = "w";
-    expect(chess.fen()).toEqual("8/8/8/8/8/8/3q4/8");
-    expect(utility(chess)).toEqual(-8);
+    expect(utility(chess)).toEqual(0);
   })
 
-  it("black should have a utility of 8", function() {
-    var chess = new Chess("8/8/8/8/8/3q4/3P4/8");
-    chess.turn = "b";
-    expect(chess.fen()).toEqual("8/8/8/8/8/8/3q4/8");
-    expect(utility(chess)).toEqual(8);
+  it("white should have a utility of 1", function() {
+      // Two kings and one white pawn
+    var chess = new Chess("3k4/3P4/8/8/8/8/8/3K4 w - - 0 1");
+    chess.turn = "w";
+    expect(utility(chess)).toEqual(1);
   })
+
+  it("black should have a utility of -1", function() {
+      // Two kings and one white pawn
+    var chess = new Chess("3k4/3P4/8/8/8/8/8/3K4 w - - 0 1");
+    chess.turn = "b";
+    expect(utility(chess)).toEqual(-1);
+  })
+
+  it("white should have a utility of 21", function() {
+    // Two kings and one of every white piece
+    var chess = new Chess("k7/8/8/8/8/8/8/PRBKQN2 w - - 0 1");
+    chess.turn = "w";
+    expect(utility(chess)).toEqual(21);
+  })
+
+  it("black should have a utility of -15", function() {
+    // Two kings and one of every white piece and one black bishop
+    var chess = new Chess("kb6/8/8/8/8/8/8/PRBKQN2 w - - 0 1");
+    chess.turn = "b";
+    expect(utility(chess)).toEqual(-18);
+  })
+
 })
