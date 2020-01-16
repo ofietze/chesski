293990bf2efe8fa57e1dec4ec2b4c38a0682f456
ofietze <oliver.fietze@akamu.de>
2020-01-16 10:47:17 +0100

    Add more test cases, not working yet

--- tests/jasmine-standalone-3.4.0/spec/testMinimax.js
+++ tests/jasmine-standalone-3.4.0/spec/testMinimax.js
@@ -1,3 +1,13 @@
+describe("negaMaxValue", function(){
+  it("black should take the pawn", function(){
+    var chess = new Chess("3k4/3P4/8/8/8/8/8/3K4 b - - 0 1");
+    chess.turn = "b";
+    var possibleMoves = chess.moves({verbose: true});
+
+    expect(possibleMoves[negaMaxValue(chess, 1)[0]]).toEqual("3");
+  });
+});
+
 describe("Minimax", function(){
 
   it("should show the next moves", function(){
@@ -7,11 +17,12 @@ describe("Minimax", function(){
   });
 
   it("black should take the pawn", function(){
-    var chess = new Chess("8/8/8/8/8/3q4/3P4/8");
+    var chess = new Chess("3k4/3P4/8/8/8/8/8/3K4 b - - 0 1");
     chess.turn = "b";
     var possibleMoves = chess.moves({verbose: true});
-
     chess.move(possibleMoves[minimaxDecision(chess, 1)]);
-    expect(chess.fen()).toEqual("8/8/8/8/8/8/3q4/8");
+
+    expect(chess.fen()).toEqual("3k4/8/8/8/8/8/8/3K4 w - - 0 1");
+    expect("\n"+chess.ascii()).toEqual("3");
   });
 });
